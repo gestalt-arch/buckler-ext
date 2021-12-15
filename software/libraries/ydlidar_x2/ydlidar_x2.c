e4dd2bccab3787430b90e31a0934ab38873b4b8c
faywerliu <faywerliu321@gmail.com>
2021-12-14 19:17:30 -0800

    lidar

--- software/libraries/ydlidar_x2/ydlidar_x2.c
+++ software/libraries/ydlidar_x2/ydlidar_x2.c
@@ -27,7 +27,7 @@ void parse_theta(YdLidarData_t* lidar) {
 
     float theta_dif = -theta_FSA + theta_LSA;
 
-    for (int i = 2; i < data_struct.LS - 1; i++) {
+    for (int i = 1; i < data_struct.LS; i++) {
         lidar->theta[INDEX] = theta_dif / (data_struct.LS - 1) * (i - 1) + theta_FSA;
     }
 
